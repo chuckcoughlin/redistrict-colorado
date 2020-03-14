0f175e936300f95da4ca0cabd952e9d8f2922b69
Chuck Coughlin <ccoughlin@ils-automation.com>
2020-03-14 10:40:55 -0600

    Getting events from tree for districts.

--- workspace/RCUI/src/redistrict/colorado/district/DistrictTreeController.java
+++ workspace/RCUI/src/redistrict/colorado/district/DistrictTreeController.java
@@ -11,6 +11,9 @@ import java.util.logging.Logger;
 import org.openjump.feature.Feature;
 import org.openjump.feature.FeatureCollection;
 
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
@@ -22,10 +25,11 @@ import redistrict.colorado.bind.BasicEventDispatcher;
 import redistrict.colorado.bind.EventReceiver;
 import redistrict.colorado.core.DatasetModel;
 import redistrict.colorado.core.DatasetRole;
+import redistrict.colorado.core.StandardAttributes;
 import redistrict.colorado.db.Database;
 import redistrict.colorado.ui.GuiUtil;
 
-public class DistrictTreeController extends StackPane implements EventReceiver<ActionEvent> {
+public class DistrictTreeController extends StackPane implements EventReceiver<ActionEvent>,ChangeListener<TreeItem<String>> {
 	private final static String CLSS = "DistrictListController";
 	private static Logger LOGGER = Logger.getLogger(CLSS);
 	private static final GuiUtil guiu = new GuiUtil();
@@ -36,6 +40,7 @@ public class DistrictTreeController extends StackPane implements EventReceiver<A
     private final Node closedFolderIcon;
 	private final BasicEventDispatcher<ActionEvent> auxEventDispatcher;
 	private final EventHandler<ActionEvent> auxEventHandler;
+	private final ReadOnlyObjectProperty<TreeItem<String>> selectionModel;
 	
 	public DistrictTreeController() {
 		this.auxEventHandler = new RegionListHolderEventHandler();
@@ -43,10 +48,12 @@ public class DistrictTreeController extends StackPane implements EventReceiver<A
 		openFolderIcon = guiu.loadImage("images/folder.png");
 		closedFolderIcon = guiu.loadImage("images/folder_closed.png");
 		this.root = new TreeItem<String> ("Datasets");
-        root.setExpanded(true);       
+        root.setExpanded(false);       
         this.tree = new TreeView<String>(root);
+        this.selectionModel = tree.getSelectionModel().selectedItemProperty();
+        selectionModel.addListener(this);
         tree.setEditable(false);
-        //tree.setCellFactory(new DistrictCellFactory());
+        tree.addEventHandler(ActionEvent.ACTION, auxEventHandler);
         populateDatasets();
         getChildren().add(tree);
 	}
@@ -65,9 +72,10 @@ public class DistrictTreeController extends StackPane implements EventReceiver<A
 				root.getChildren().add(item);
 				FeatureCollection collection = datasetModel.getFeatures();
 				if(collection!=null) {
-					//String nameAttribute = Database.getInstance().getAttributeAliasTable().nameForAlias(id, alias)
+					String nameAttribute = Database.getInstance().getAttributeAliasTable().nameForAlias(datasetModel.getId(), StandardAttributes.ID.name());
 					for(Feature feature:collection.getFeatures() ) {
-						//TreeItem<String> leaf = new TreeItem<String> (feature.getAttribute("Name").toString());
+						TreeItem<String> leaf = new TreeItem<String> (feature.getAttribute(nameAttribute).toString());
+						item.getChildren().add(leaf);
 					}
 				}
 			}
@@ -77,7 +85,16 @@ public class DistrictTreeController extends StackPane implements EventReceiver<A
 	public BasicEventDispatcher<ActionEvent> getAuxillaryEventDispatcher() {
 		return auxEventDispatcher;
 	}
-	
+
+	// ================================================ Change Listener =========================================
+	@Override
+	public void changed(ObservableValue<? extends TreeItem<String>> observable, TreeItem<String> oldValue,TreeItem<String> newValue) {
+		TreeItem<String> selectedItem = (TreeItem<String>) newValue;
+		LOGGER.info(String.format("%s.changed %s: Arg 1 = %s Arg2 = %s", CLSS,observable,oldValue,newValue));
+	}
+
+
+	// ================================================ Event Handler =========================================
 	/**
 	 * We've received an event from the button panel (or other). React.
 	 */
@@ -87,5 +104,6 @@ public class DistrictTreeController extends StackPane implements EventReceiver<A
 			LOGGER.info(String.format("%s.handle: Action event: source = %s", CLSS,((Node)event.getSource()).getId()));
 		}
 	}
+
 	
 }
